a7f1445d6a19b6ce4f15ff46aa25b3900093153b
diadolyy <niceline111@gmail.com>
2025-02-19 19:37:42 +0300

    добавлены стили и индикатор загрузки

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 document.getElementById("loadBtn").addEventListener("click", async function() {
     const userListDiv=document.getElementById("userList");
+    const loadingDiv=document.getElementById("loading");
+
+    loadingDiv.style.display='block';
     try{
         const response=await fetch("https://jsonplaceholder.typicode.com/users");
         const users=await response.json();
 
-        userListDiv.innerHTML="<ul>"+users.map(user=> `<li>${user.name}</li>`);
+        loadingDiv.style.display='none';
+        userListDiv.innerHTML="<ul>"+users.map(user=> `<li>${user.name}</li>`).join('')+"</ul>";
 
     }catch(error){
+        loadingDiv.style.display='none';
         userListDiv.innerHTML="Произошла ошибка при загрузке данных";
         console.error("Ошибка:", error);
     }
